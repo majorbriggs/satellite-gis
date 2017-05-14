6d916434432fda86198efc04d0c841e30e307e74
piotrek <pbaranowski1@gmail.com>
2017-05-14 23:31:51 +0200

    don't create folders

--- geoserver_utils.py
+++ geoserver_utils.py
@@ -3,16 +3,13 @@ from geoserver_api import add_coverage_store, add_layer
 import os
 
 GEOSERVER_DATA_DIR = "/home/ubuntu/satellite-gis/"
+#GEOSERVER_DATA_DIR = "/home/piotrek/data/"
 
 def download_from_s3_and_add_layer(image_key):
-    image_dir = GEOSERVER_DATA_DIR + image_key.split('/')[0]
-
-    data_store_name = image_key.replace('/', '_')
+    data_store_name = image_key.replace('/', '')
     filepath = GEOSERVER_DATA_DIR + data_store_name
-    if not os.path.exists(image_dir):
-        os.mkdir(image_dir)
     s3_download_file(image_key, filepath)
     data_source_path = "file://" + filepath
     add_coverage_store('sentinel', data_store_name, path=data_source_path)
-    add_layer(ws='sentinel', cs=data_store_name, name=filepath, title="rgb")
+    add_layer(ws='sentinel', cs=data_store_name, name=data_store_name, title=data_store_name)
     return True
