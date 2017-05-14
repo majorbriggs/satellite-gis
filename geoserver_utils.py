c6244e1e398af0e5c55b49d48752adc8eb59a6ce
piotrek <pbaranowski1@gmail.com>
2017-05-14 22:25:10 +0200

    change paths of layer files

--- geoserver_utils.py
+++ geoserver_utils.py
@@ -6,12 +6,13 @@ GEOSERVER_DATA_DIR = "/home/ubuntu/satellite-gis/"
 
 def download_from_s3_and_add_layer(image_key):
     image_dir = GEOSERVER_DATA_DIR + image_key.split('/')[0]
-    filepath = GEOSERVER_DATA_DIR + image_key
+
     data_store_name = image_key.replace('/', '_')
+    filepath = GEOSERVER_DATA_DIR + data_store_name
     if not os.path.exists(image_dir):
         os.mkdir(image_dir)
     s3_download_file(image_key, filepath)
     data_source_path = "file://" + filepath
     add_coverage_store('sentinel', data_store_name, path=data_source_path)
-    add_layer(ws='sentinel', cs=data_store_name, name="rgb", title="rgb")
+    add_layer(ws='sentinel', cs=data_store_name, name=filepath, title="rgb")
     return True
